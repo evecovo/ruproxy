417968a9445547badd0d3d565eca611c9b88ce5e
163jm <fujm8527@163.com>
2026-05-14 10:22:38 +0800

    Update server.rs

--- src/hysteria2/server.rs
+++ src/hysteria2/server.rs
@@ -383,9 +383,7 @@ async fn masquerade_proxy(
 ) -> MasqResponse {
     use http_body_util::{BodyExt, Empty};
     use hyper::body::Bytes as HBytes;
-    use hyper::header::{
-        CONTENT_LENGTH, CONTENT_TYPE, HOST, LOCATION, TRANSFER_ENCODING,
-    };
+    use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE, HOST, LOCATION, TRANSFER_ENCODING};
     use hyper_util::client::legacy::Client;
     use hyper_util::rt::TokioExecutor;
 
